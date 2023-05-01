0cea7aebc702145f58aa700a150635f38d93e71e
Sushant Thyagaraj <sushantt@gmail.com>
2023-05-01 10:53:49 -0700

    Added trainer file with GlowTTS model

---
+++ train.py
@@ -0,0 +1,78 @@
+import os
+
+# Trainer: Where the ✨️ happens.
+# TrainingArgs: Defines the set of arguments of the Trainer.
+from trainer import Trainer, TrainerArgs
+
+# GlowTTSConfig: all model related values for training, validating and testing.
+from TTS.tts.configs.glow_tts_config import GlowTTSConfig
+
+# BaseDatasetConfig: defines name, formatter and path of the dataset.
+from TTS.tts.configs.shared_configs import BaseDatasetConfig
+from TTS.tts.datasets import load_tts_samples
+from TTS.tts.models.glow_tts import GlowTTS
+from TTS.tts.utils.text.tokenizer import TTSTokenizer
+from TTS.utils.audio import AudioProcessor
+
+output_path = "dataset"
+if not os.path.exists(output_path):
+    os.makedirs(output_path)
+
+
+def train():
+    dataset_config = BaseDatasetConfig(
+        formatter="ljspeech",
+        meta_file_train="metadata.csv",
+        path=os.path.join(output_path, "LJSpeech-1.1/"),
+    )
+
+    config = GlowTTSConfig(
+        batch_size=32,
+        eval_batch_size=16,
+        num_loader_workers=4,
+        num_eval_loader_workers=4,
+        run_eval=True,
+        test_delay_epochs=-1,
+        epochs=100,
+        text_cleaner="phoneme_cleaners",
+        use_phonemes=True,
+        phoneme_language="en-us",
+        phoneme_cache_path=os.path.join(output_path, "phoneme_cache"),
+        print_step=25,
+        print_eval=False,
+        mixed_precision=True,
+        output_path=output_path,
+        datasets=[dataset_config],
+        save_step=1000,
+        eval_split_size=10,
+    )
+
+    ap = AudioProcessor.init_from_config(config)
+    # Modify sample rate if for a custom audio dataset:
+    ap.sample_rate = 48000
+
+    tokenizer, config = TTSTokenizer.init_from_config(config)
+
+    train_samples, eval_samples = load_tts_samples(
+        dataset_config,
+        eval_split=True,
+        eval_split_max_size=config.eval_split_max_size,
+        eval_split_size=config.eval_split_size,
+    )
+
+    model = GlowTTS(config, ap, tokenizer, speaker_manager=None)
+
+    trainer = Trainer(
+        TrainerArgs(),
+        config,
+        output_path,
+        model=model,
+        train_samples=train_samples,
+        eval_samples=eval_samples,
+    )
+
+    trainer.fit()
+
+
+if __name__ == "__main__":
+    train()
