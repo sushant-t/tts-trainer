7d5f7fa48fe02ac01463937b43b00d21941910fb
Sushant Thyagaraj <sushantt@gmail.com>
2023-05-12 00:46:57 -0700

    Modified setup.sh and moved all Python dependencies into a requirements.txt file.

--- setup.sh
+++ setup.sh
@@ -3,8 +3,4 @@ eval "$(conda shell.bash hook)"
 conda activate tts-trainer
 mamba install -y python=3.10
 mamba install -y pip
-mamba install -y pandas
-mamba install -y numpy=1.23.1
-mamba install -y pydub
-pip install TTS
-pip install -U openai-whisper
+pip install --upgrade --force-reinstall -r requirements.txt
