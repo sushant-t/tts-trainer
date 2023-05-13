2f703adc912a1f0f493692c10280092d0df4b4a1
Sushant Thyagaraj <sushantt@gmail.com>
2023-05-13 10:22:10 -0700

    Cleaned up audio trimming workflow from diarization.

--- src/utils/audio_cleaning.py
+++ src/utils/audio_cleaning.py
@@ -1,6 +1,4 @@
 ## Clean and enhance raw audio
-import matplotlib.pyplot as plt
-import numpy as np
 from pydub import AudioSegment
 import os
 import librosa
@@ -25,7 +23,6 @@ def convert_to_wav(audio_path):
 
 def filter_noise(audio_path):
     data, sampling_rate = librosa.load(audio_path)
-    # y = filter_audio_frequencies(data, 300.0, 4000.0, sampling_rate, order=6)
     reduced_noise = nr.reduce_noise(y=data, sr=sampling_rate, prop_decrease=0.7)
 
     dir_name = os.path.dirname(audio_path)
@@ -38,27 +35,15 @@ def filter_noise(audio_path):
     return output_path
 
 
-def trim_audio_from_diarization(wav_path, speaker_turns):
-    audio = AudioSegment.from_file(wav_path)
+def trim_audio_from_diarization(audio, speaker_turns):
     out = AudioSegment.empty()
     for turn in speaker_turns:
         clip = audio[turn["start"] * 1000 : turn["end"] * 1000]
         out = out.append(clip, crossfade=0)
 
-    print(audio.duration_seconds, out.duration_seconds)
+    print("Raw Audio vs. Trimmed Audio:", audio.duration_seconds, out.duration_seconds)
 
-    dir_name = os.path.dirname(wav_path)
-    file_name = os.path.splitext(os.path.basename(wav_path))[0]
-    ext = ".wav"
-    output_path = os.path.join(dir_name, file_name + "_trimmed" + ext)
-
-    normalized_audio: AudioSegment = normalize_audio(out, -20.0)
-
-    normalized_audio.export(
-        output_path,
-        format="wav",
-    )
-    return output_path
+    return out
 
 
 def normalize_audio(aChunk, target_dBFS):
@@ -71,3 +56,17 @@ def prepare_chunk_for_training(audio_segment: AudioSegment):
     audio_segment = audio_segment.set_frame_rate(22050)
     audio_segment = audio_segment.set_channels(1)
     return audio_segment
+
+
+def save_trimmed_audio(wav_path, audio_segment):
+    dir_name = os.path.dirname(wav_path)
+    file_name = os.path.splitext(os.path.basename(wav_path))[0]
+    ext = ".wav"
+    output_path = os.path.join(dir_name, file_name + "_trimmed" + ext)
+
+    audio_segment.export(
+        output_path,
+        format="wav",
+    )
+
+    return output_path
