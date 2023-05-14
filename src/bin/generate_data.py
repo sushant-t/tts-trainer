1551260fc817df219177c6bf54382229a5f8cff7
Sushant Thyagaraj <sushantt@gmail.com>
2023-05-13 19:08:42 -0700

    Added option to not overwrite dataset files, so that newly added raw samples can be appended to the dataset instead of starting over.

--- src/bin/generate_data.py
+++ src/bin/generate_data.py
@@ -1,51 +1,45 @@
 from src.config.definitions import ROOT_DIR
-from src.diarization.diarize import generate_speaker_timestamps
 from src.utils.audio_cleaning import (
-    filter_noise,
-    trim_audio_from_diarization,
-    normalize_audio,
-    save_trimmed_audio,
+    is_audio_file,
     organize_input_samples,
     rename_input_samples,
+    clean_audio,
 )
 from src.utils.smart_splitting import process_split
 from src.utils.transcribe import transcribe_all
-from pydub import AudioSegment
 import os
 import argparse
+import re
 
 
-def clean_audio(audio_path):
-    output_path = filter_noise(audio_path)
-
-    filtered_speaker_turns = generate_speaker_timestamps(output_path)
-
-    audio = AudioSegment.from_file(output_path)
-    audio = normalize_audio(audio, -20.0)
-    trimmed_output = trim_audio_from_diarization(audio, filtered_speaker_turns)
-
-    trimmed_output_path = save_trimmed_audio(output_path, trimmed_output)
-
-    return trimmed_output_path
-
-
-def generate_data(folder):
+def generate_data(**kwargs):
+    folder = kwargs["input_folder"]
+    no_overwrite = kwargs["no_overwrite"]
     organize_input_samples(folder)
     speaker_name = os.path.basename(folder)
     rename_input_samples(speaker_name, folder)
-
-    for file in os.listdir(folder):
-        path = os.path.join(folder, file)
+    wavs_path = os.path.join(ROOT_DIR, "./dataset/{0}/wavs".format(speaker_name))
+    generated_files = set(
+        [re.search(r"(.*?)_[0-9]+", f).group() for f in os.listdir(wavs_path)]
+    )
+    raw_folder = os.path.join(folder, "raw")
+    for file in os.listdir(raw_folder):
+        if not is_audio_file(file):
+            continue
+        if (
+            no_overwrite
+            and os.path.splitext(os.path.basename(file))[0] in generated_files
+        ):
+            continue
+        path = os.path.join(raw_folder, file)
         print("cleaning audio file")
         clean_path = clean_audio(path)
 
         print("splitting file...")
-        process_split(clean_path)
-
-        base_name = os.path.splitext(os.path.basename(clean_path))[0]
+        split_path = process_split(clean_path)
 
         print("transcribing file segments...")
-        transcribe_all(base_name)
+        transcribe_all(split_path)
 
 
 if __name__ == "__main__":
@@ -54,8 +48,18 @@ if __name__ == "__main__":
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument("-i", "--input_folder", help="input audio folder")
+    parser.add_argument(
+        "--no_overwrite",
+        action=argparse.BooleanOptionalAction,
+        help="do not overwrite any duplicates in dataset",
+    )
     args = parser.parse_args()
-    if args.input_folder and os.path.exists(args.input_folder):
-        generate_data(args.input_folder)
+    full_path = (
+        os.path.join(ROOT_DIR, args.input_folder)
+        if os.path.exists(os.path.join(ROOT_DIR, args.input_folder))
+        else os.path.exists(os.path.join(os.getcwd(), args.input_folder))
+    )
+    if args.input_folder and os.path.exists(full_path):
+        generate_data(input_folder=args.input_folder, no_overwrite=args.no_overwrite)
     else:
         print("Invalid input file")
